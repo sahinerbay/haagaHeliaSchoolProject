34716bb0b1e7882f33837d20bf7603041d118003
sahinerbay <sahin@sahinerbay.com>
2017-09-11 22:46:18 +0300

    autoprefixer, browser-sync and sass added through gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync').create(),
+  autoprefixer = require('autoprefixer');
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions'],
+  cascade: false
+};
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass:watch', function () {
+
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+  
+});
