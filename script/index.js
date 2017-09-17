222cf519510d873bcb1a973a235f757f6be98d60
sahinerbay <sahin@sahinerbay.com>
2017-09-18 01:08:34 +0300

    button color changed

--- script/index.js
+++ script/index.js
@@ -1,4 +1,30 @@
-'use strict';
+"use strict";
+
+var getData = function getData(url, callback) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        timeout: 2000
+    }).done(function (result) {
+        callback(result);
+    }).fail(function (textStatus, jqXHR) {
+        alert("Request failed: " + textStatus);
+    }).always(function () {
+        console.log("this will always be executed");
+    });
+};
+
+var todaysFormattedDate = function todaysFormattedDate() {
+    var d = new Date(),
+        curr_date = d.getDate(),
+        curr_month = d.getMonth() + 1,
+        curr_year = d.getFullYear();
+    if (curr_month < 10) {
+        curr_month = '0' + curr_month;
+    }
+    var todaysDate = curr_year + "-" + curr_month + "-" + curr_date;
+    return todaysDate;
+};
 
 $(function () {
     var hideElements = function hideElements() {
@@ -7,9 +33,9 @@ $(function () {
         }
 
         for (var index = 0; index < args.length; index++) {
-            var $element = $('.' + args[index]);
-            if ($element.hasClass('haaga__small--width-100')) {
-                element.removeClass('haaga__small--width-100');
+            var $element = $("." + args[index]);
+            if ($element.hasClass('haaga__small--width-25')) {
+                $element.removeClass('haaga__small--width-25');
             }
             $element.addClass('haaga__small--width-0');
         }
@@ -21,14 +47,38 @@ $(function () {
         }
 
         for (var index = 0; index < args.length; index++) {
-            $('.' + args[index]).addClass('haaga__small--width-100');
+            $("." + args[index]).addClass('haaga__small--width-25');
         }
     };
 
-    var amicaButton = $('.amica__center__button');
-    var haaga = $('.haaga');
-    amicaButton.on('click', function (e) {
-        e.preventDefault();
+    var createNode = function createNode(element) {
+        return document.createElement(element);
+    };
+
+    var createTextNode = function createTextNode(text) {
+        return document.createTextNode(text);
+    };
+
+    var $amicaButton = $('.amica__center__button');
+
+    $amicaButton.on('click', function (event) {
+        event.preventDefault();
+        $('.amica').addClass('amica__small--width-25 amica--border');
         hideElements('haaga');
+        $(this).fadeOut(1000); //hides the clicked button
+
+        var p = createNode('p'),
+            text = createTextNode('Helia-Bistro is a student café located in Haaga-Helia, Pasila, in Helsinki, next to Helsinki Fair Centre. Every day, we serve vegetarian dish, vegetarian soup and salad lunches. Students eating lunch at Helia-Bistro are entitled to a Kela meal subsidy.');
+        p.appendChild(text);
+
+        $('.amica__center img').after(p);
+        var data = void 0;
+        var d = new Date();
+        getData('http://www.amica.fi/modules/json/json/Index?costNumber=0084&language=en', function (result) {
+            data = result.MenusForDays;
+            return data;
+        });
+
+        console.log(data);
     });
 });
--- scriptES6/index.js
+++ scriptES6/index.js
@@ -1,9 +1,35 @@
+let getData = (url, callback) => {
+    $.ajax({
+        type: "GET",
+        url: url,
+        timeout: 2000
+    }).done(function (result) {
+        callback(result);
+    }).fail(function (textStatus, jqXHR) {
+        alert("Request failed: " + textStatus)
+    }).always(function () {
+        console.log("this will always be executed");
+    });
+};
+
+let todaysFormattedDate = () => {
+	let d = new Date(),
+  		curr_date = d.getDate(),
+      curr_month = d.getMonth() + 1,
+      curr_year = d.getFullYear();
+  if( curr_month < 10) {
+		curr_month = '0' + curr_month;  
+  }
+  let todaysDate = `${curr_year}-${curr_month}-${curr_date}`;
+  return todaysDate;
+};
+
 $(function () {
     let hideElements = (...args) => {
         for (let index = 0; index < args.length; index++) {
             let $element = $(`.${args[index]}`);
-            if($element.hasClass('haaga__small--width-100')) {
-                element.removeClass('haaga__small--width-100');
+            if ($element.hasClass('haaga__small--width-25')) {
+                $element.removeClass('haaga__small--width-25');
             }
             $element.addClass('haaga__small--width-0');
         }
@@ -11,17 +37,46 @@ $(function () {
 
     let showElements = (...args) => {
         for (let index = 0; index < args.length; index++) {
-            $(`.${args[index]}`).addClass('haaga__small--width-100');
+            $(`.${args[index]}`).addClass('haaga__small--width-25');
         }
-    }
+    };
 
-    let amicaButton = $('.amica__center__button');
-    let haaga = $('.haaga');
-    amicaButton.on('click', function (e) {
-        e.preventDefault();
+    let createNode = (element) => {
+        return document.createElement(element);
+    };
+
+    let createTextNode = (text) => {
+        return document.createTextNode(text);
+    };
+
+    let $amicaButton = $('.amica__center__button');
+
+
+    $amicaButton.on('click', function (event) {
+        event.preventDefault();
+        $('.amica').addClass('amica__small--width-25 amica--border');
         hideElements('haaga');
+        $(this).fadeOut(1000); //hides the clicked button
+
+        let p = createNode('p'),
+            text = createTextNode('Helia-Bistro is a student café located in Haaga-Helia, Pasila, in Helsinki, next to Helsinki Fair Centre. Every day, we serve vegetarian dish, vegetarian soup and salad lunches. Students eating lunch at Helia-Bistro are entitled to a Kela meal subsidy.');
+        p.appendChild(text);
+
+        $('.amica__center img').after(p);
+        let data; 
+        var d = new Date();
+        getData('http://www.amica.fi/modules/json/json/Index?costNumber=0084&language=en', (result)=> {
+            data = result.MenusForDays;
+            return data;
+        });
+
+        console.log(data)
+        
     })
 
-    
+
 
 });
+
+
+
